a2029051b446c54a8dea33ace863a184209e1901
SandlerRodrigo <rodrigoqsandler@gmail.com>
2025-11-03 19:55:52 -0300

    Initial Commit

---
+++ POC-add-connection/POC_add_connectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_add_connectionApp.swift
+//  POC-add-connection
+//
+//  Created by Rodrigo Sandler on 03/11/25.
+//
+
+import SwiftUI
+
+@main
+struct POC_add_connectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
